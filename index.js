763c45ba8a2e04a7647aa752a9077d92aeb114c6
akilank-tech <akilank1601@gmail.com>
2026-09-13 13:16:05 +0530

    update navbar

--- index.js
+++ index.js
@@ -4,3 +4,12 @@ remove.addEventListener("click",function(){
     offtag.style.display="none";
 })
 
+var bar = document.getElementById("bar")
+var sidenavbar = document.getElementById("sidenavbar")
+var closebtn = document.querySelector(".close-btn")
+bar.addEventListener("click",function(){
+    sidenavbar.style.left="0";
+})
+closebtn.addEventListener("click",function(){
+    sidenavbar.style.left="-50%";
+})
